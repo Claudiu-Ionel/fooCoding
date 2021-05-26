316c82e56ff598c63ae1b42f00a32063ec62e0e3
Claudiu Ionel <55031343+Claudiu-Ionel@users.noreply.github.com>
2021-05-26 20:14:45 +0200

    changed the condition to %land%
    Changed the condition to %land% so that the query fetches all the countries that contain "land" in the name

--- Database/Week1/new_world-Claudiu_Ionel.sql
+++ Database/Week1/new_world-Claudiu_Ionel.sql
@@ -1,6 +1,6 @@
 use new_world;
 SELECT Name, Population FROM country WHERE Population > '8000000';
-SELECT Name FROM country WHERE Name LIKE '%land';
+SELECT Name FROM country WHERE Name LIKE '%land%';
 SELECT Name, Population FROM city WHERE Population BETWEEN '500000' AND '1000000';
 SELECT Name, Continent FROM country WHERE Continent = 'Europe';
 SELECT Name, SurfaceArea FROM country ORDER BY SurfaceArea DESC;
